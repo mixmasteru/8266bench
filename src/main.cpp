44533b81d492e4ec2f1908f3ee03df14b5e1543e
Ulrich Pech <ulrichpech@gmail.com>
2016-10-01 21:24:05 +0200

    AES test

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,57 @@
-// BigNumber test: factorials
 #include "Arduino.h"
 #include "BigNumber.h"
+#include "AES.h"
+
+AES aes;
+byte plain[] = "Add NodeAdd NodeAdd NodeAdd NodeAdd Node";
+byte *key = (unsigned char*)"0123456789010123";
+unsigned long long int my_iv = 36753562;
+
+void printArray(byte b[],bool p_pad)
+{
+    uint8_t i,j;
+    uint8_t loops = aes.get_size()/N_BLOCK;
+    uint8_t outp = N_BLOCK;
+    for (j = 0; j < loops; j += 1){
+      if (p_pad && (j == (loops  - 1)) ) { outp = N_BLOCK - aes.get_pad(); }
+      for (i = 0; i < outp; i++)
+      {
+        Serial.print((char)b[j*N_BLOCK + i]);
+      }
+    }
+  }
+
+void prekey (int bits)
+{
+  aes.iv_inc();
+  byte iv [N_BLOCK] ;
+  byte plain_p[48];
+  byte cipher [48] ;
+  byte check [48] ;
+  unsigned long ms = micros ();
+  aes.set_IV(my_iv);
+  aes.get_IV(iv);
+  aes.do_aes_encrypt(plain,41,cipher,key,bits,iv);
+  Serial.print("Encryption took: ");
+  Serial.println(micros() - ms);
+  ms = micros ();
+  aes.set_IV(my_iv);
+  aes.get_IV(iv);
+  aes.do_aes_decrypt(cipher,48,check,key,bits,iv);
+  Serial.print("Decryption took: ");
+  Serial.println(micros() - ms);
+  Serial.print("\n\nPLAIN :");
+  printArray(plain,true);
+  Serial.print("\nCIPHER:");
+  printArray(cipher,false);
+  Serial.print("\nCHECK :");
+  printArray(check,true);
+  Serial.print("\nIV    :");
+  aes.printArray(iv,16);
+  Serial.print("\n============================================================\n");
+}
+
+
 
 void calcE(int ini)
 {
@@ -65,12 +116,13 @@ void setup ()
   Serial.println ();
   delay(5000);
 
-  calcE(283);
+  //calcE(283);
 
 
 }  // end of setup
 
 void loop ()
 {
-
+  prekey (128) ;
+  delay(2000);
 }
